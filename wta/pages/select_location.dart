1eee506b58b9c4fa6448e8d9e7bfab3446b0b0b6
HarshitGulgulia <harshitgulgulia@gmail.com>
2022-01-31 18:49:44 +0530

    Added list of locations

--- wta/pages/select_location.dart
+++ wta/pages/select_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wta/services/World_Time.dart';
 
 class SelectLocation extends StatefulWidget {
   const SelectLocation({Key? key}) : super(key: key);
@@ -9,16 +10,50 @@ class SelectLocation extends StatefulWidget {
 
 class _SelectLocationState extends State<SelectLocation> {
 
+  List<WorldTime> locations = [
+    WorldTime(location: 'London', flag: 'uk.png', url: 'Europe/London'),
+    WorldTime(location: 'Athens', flag: 'greece.png', url: 'Europe/Berlin'),
+    WorldTime(location: 'Cairo', flag: 'egypt.png', url: 'Africa/Cairo'),
+    WorldTime(location: 'Nairobi', flag: 'kenya.png', url: 'Africa/Nairobi'),
+    WorldTime(location: 'Chicago', flag: 'usa.png', url: 'America/Chicago'),
+    WorldTime(location: 'New York', flag: 'usa.png', url: 'America/New_York'),
+    WorldTime(location: 'Seoul', flag: 'south_korea.png', url: 'Asia/Seoul'),
+    WorldTime(location: 'Jakarta', flag: 'indonesia.png', url: 'Asia/Jakarta'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     print('build function ran');
     return Scaffold(
-      backgroundColor: Colors.amber,
+      backgroundColor: Colors.lightBlue,
       appBar: AppBar(
-        backgroundColor: Colors.pinkAccent,
+        backgroundColor: Colors.blue,
         title: Text('Select a Location'),
         centerTitle: true,
         elevation: 0,
+      ),
+      body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index){
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+              child: Card(
+                color: Colors.lightBlueAccent,
+                child: ListTile(
+                  onTap: () {
+                    print(locations[index].location);
+                  },
+                  title: Text(locations[index].location),
+                  textColor: Colors.white,
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      'assets/${locations[index].flag}'
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
       ),
     );
   }
