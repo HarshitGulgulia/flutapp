90dbe91c7d66881d75d4a8571bf3cc6214f3e833
HarshitGulgulia <harshitgulgulia@gmail.com>
2022-01-31 13:34:26 +0530

    Added different backgrounds for day and night

--- wta/pages/home.dart
+++ wta/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
   _HomeState createState() => _HomeState();
 }
@@ -15,39 +17,63 @@ class _HomeState extends State<Home> {
     data = ModalRoute.of(context)!.settings.arguments as Map;
     print(data);
 
+    //Set Background
+    String bgImage = data['isDaytime'] ? 'Day.jpg' : 'Night.jpg';
+    Color bgColor = data['isDaytime'] ? Colors.blueGrey : Colors.indigoAccent;
+    Color lcColor = data['isDaytime'] ? Colors.black87 : Colors.white;
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-          child: Column(
-            children: [
-              TextButton.icon(onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location'),
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                      data['location'],
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 135, 0, 0),
+            child: Column(
+              children: [
+                TextButton.icon(onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                  icon: Icon(
+                      Icons.edit_location,
+                      color: lcColor,
+                  ),
+                  label: Text(
+                      'Edit Location',
                       style: TextStyle(
-                        fontSize: 28,
-                        letterSpacing: 2,
+                        color: lcColor,
                       ),
-                  )
-                ],
-              ),
-              SizedBox(height: 20),
-              Text(
-                data['time'],
-                style: TextStyle(
-                  fontSize: 66,
+                  ),
                 ),
-              )
-            ],
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                        data['location'],
+                        style: TextStyle(
+                          fontSize: 36,
+                          letterSpacing: 2,
+                          color: lcColor,
+                        ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66,
+                    color: lcColor,
+                  ),
+                )
+              ],
+            ),
           ),
         ),),
     );
